395acff3ac89f69682a43a5970efa25652ec8a20
anthonyQA <anthonymark0023@gmail.com>
2024-01-03 16:28:43 +0800

    Added Test Scenarios for login and log api modules.

---
+++ automation-dph/TestScenarios/Login.cs
@@ -0,0 +1,100 @@
+﻿namespace automation_dph.TestScenarios
+{
+    public class Login : BasePageDriver
+    {
+        public Login(IPage page, PageTest pageTest) : base(page, pageTest)
+        {
+
+        }
+
+        public const string ScreenShotFolderName = "Login - Screenshot";
+
+        public string[] TestScenarios = new string[]
+        {
+            "1.1 Validate Valid Username and Valid Password",
+            "1.2 Validate Valid Username and Invalid Password",
+            "1.3 Validate Invalid Username and valid Password",
+            "1.4 Validate Invalid Username and invalid Password",
+            "1.5 Validate Checked/Unchecked Remember username",
+            "1.6 Validate Forgot Password? Hyperlink",
+            "1.7 Validate Valid email for Reset Password",
+            "1.8 Validate Blank value in Email field",
+            "1.9 Validate Blank value in Password field",
+            "1.12 Validate Blank value in Reset page Email field",
+        };
+
+        public string[] StepsToReplicate = new string[]
+        {
+            // 1.1
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Enter Valid username in email textfield.<br>" +
+                        "2. Enter Valid password in password textfield.<br>" +
+                        "3. Click login button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should be login without any errors.<br> " +
+                        "Succes notification should display.<br><br>",
+            // 1.2
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Enter Valid username in email textfield.<br>" +
+                        "2. Enter Invalid password in password textfield.<br>" +
+                        "3. Click login button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should not be login.<br> " +
+                        "Error notification should display.<br><br>",
+            // 1.3
+            "<b>Steps to Replicate:</b><br>" +
+                         "1. Enter Invalid username in email textfield.<br>" +
+                         "2. Enter Valid password in password textfield.<br>" +
+                         "3. Click login button.<br><br>" +
+                         "<b>Expected Result:</b><br> User should not be login.<br> " +
+                         "Error notification should display.<br><br>",
+            // 1.4
+            "<b>Steps to Replicate:</b><br>" +
+                         "1. Enter Invalid username in email textfield.<br>" +
+                         "2. Enter Invalid password in password textfield.<br>" +
+                         "3. Click login button.<br><br>" +
+                         "<b>Expected Result:</b><br> User should not be login.<br> " +
+                         "Error notification should display.<br><br>",
+            // 1.5
+            "<b>Steps to Replicate:</b><br>" +
+                         "1. Enter Valid username in email textfield.<br>" +
+                         "2. Enter Valid password in password textfield.<br>" +
+                         "3. Click Remember username checkbox.<br>" +
+                         "4. Click login button.<br>" +
+                         "5. Click profile icon, then click logout button.<br>" +
+                         "6. Verify value username in email textfield.<br><br>" +
+                         "<b>Expected Result:</b><br> Value should display based on the input.<br><br>",
+            // 1.6
+            "<b>Steps to Replicate:</b><br>" +
+                         "1. Click Forgot Password? hyper link.<br>" +
+                         "2. Enter value in email textfield.<br><br>" +
+                         "<b>Expected Result:</b><br> Inputted value should display.<br><br>",
+            // 1.7
+            "<b>Steps to Replicate:</b><br>" +
+                         "1. Click Forgot Password? hyper link.<br>" +
+                         "2. Enter value in email textfield.<br>" +
+                         "3. Click Reset Password button.<br><br>" +
+                         "<b>Expected Result:</b><br> Succcess notification should display.<br>" +
+                         "Reset password link should send based on email inputted.<br><br>",
+            // 1.8
+            "<b>Steps to Replicate:</b><br>" +
+                         "1. Enter Blank value in email textfield.<br>" +
+                         "2. Enter Valid password in password textfield.<br>" +
+                         "3. Click login button.<br><br>" +
+                         "<b>Expected Result:</b><br> User should not be login.<br>" +
+                         "Error notification should display.<br><br>",
+            // 1.9
+            "<b>Steps to Replicate:</b><br>" +
+                         "1. Enter Valid value in email textfield.<br>" +
+                         "2. Enter Blank password in password textfield.<br>" +
+                         "3. Click login button.<br><br>" +
+                         "<b>Expected Result:</b><br> User should not be login.<br>" +
+                         "Error notification should display.<br><br>",
+            // 1.12
+            "<b>Steps to Replicate:</b><br>" +
+                         "1. Click Forgot Password? hyper link.<br>" +
+                         "2. Enter Blank value in email textfield.<br>" +
+                         "3. Click Reset Password button.<br><br>" +
+                         "<b>Expected Result:</b><br> Error notification should display.<br>" +
+                         "Reset password link should not sent.<br><br>",
+        };
+    }
+}
---
+++ automation-dph/TestScenarios/LoginApi.cs
@@ -0,0 +1,82 @@
+﻿namespace automation_dph.TestScenarios
+{
+    public class LoginApi : BasePageDriver
+    {
+        public LoginApi(IPage page, PageTest pageTest) : base(page, pageTest)
+        {
+
+        }
+
+        public static string[] TestScenarios = new string[]
+        {
+            "1.1 Validate Valid Username and Valid Password",
+            "1.2 Validate Valid Username and Invalid Password",
+            "1.3 Validate Invalid Username and valid Password",
+            "1.4 Validate Invalid Username and invalid Password",
+            "1.5 Validate Blank value in Email field.",
+            "1.6 Validate Blank value in Password field.",
+            "1.7 Validate Max length value in Email field.",
+            "1.8 Validate Max length value in Password field.",
+        };
+
+        public static string[] StepsToReplicate = new string[]
+        {
+            // 1.1
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Fill-up a Valid username in email field.<br>" +
+                        "2. Fill-up a Valid password in password field.<br>" +
+                        "3. Click Send button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should be login without any errors.<br> " +
+                        "Response should be true.<br><br>",
+            // 1.2
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Fill-up a Valid username in email field.<br>" +
+                        "2. Fill-up a Invalid password in password field.<br>" +
+                        "3. Click Send button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should not login.<br> " +
+                        "Response should be INVALID_PASSWORD.<br><br>",
+            // 1.3
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Fill-up a Invalid username in email field.<br>" +
+                        "2. Fill-up a Valid password in password field.<br>" +
+                        "3. Click Send button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should not login.<br> " +
+                        "Response should be EMAIL_NOT_FOUND.<br><br>",
+            // 1.4
+           "<b>Steps to Replicate:</b><br>" +
+                        "1. Fill-up a Invalid username in email field.<br>" +
+                        "2. Fill-up a Invalid password in password field.<br>" +
+                        "3. Click Send button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should not login.<br> " +
+                        "Response should be EMAIL_NOT_FOUND.<br><br>",
+            // 1.5
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Fill-up a blank value in email field.<br>" +
+                        "2. Fill-up a valid value in password field.<br>" +
+                        "3. Click Send button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should not login.<br> " +
+                        "Response should be Validation Error.<br><br>",
+            // 1.6
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Fill-up a valid value in email field.<br>" +
+                        "2. Fill-up a blank value in password field.<br>" +
+                        "3. Click Send button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should not login.<br> " +
+                        "Response should be Validation Error.<br><br>",
+            // 1.7
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Fill-up a max length value in email field.<br>" +
+                        "2. Fill-up a valid value in password field.<br>" +
+                        "3. Click Send button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should not login.<br> " +
+                        "Response should be Validation Error.<br><br>",
+            // 1.8
+            "<b>Steps to Replicate:</b><br>" +
+                        "1. Fill-up a valid value in email field.<br>" +
+                        "2. Fill-up a max length value in password field.<br>" +
+                        "3. Click Send button.<br><br>" +
+                        "<b>Expected Result:</b><br> User should not login.<br> " +
+                        "Response should be Validation Error.<br><br>",
+        };
+    }
+}
