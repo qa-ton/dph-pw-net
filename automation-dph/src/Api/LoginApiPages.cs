ad2b1fa0d71929f11b36eeace6d52796f56bed94
anthonyQA <anthonymark0023@gmail.com>
2024-01-03 16:32:08 +0800

    Added LoginApiPages and LoginResponseField.

---
+++ automation-dph/src/Api/LoginApiPages.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Playwright;
+
+namespace automation_dph.src.Api
+{
+    public class LoginApiPages : BasePageDriver
+    {
+        public LoginApiPages(IPage page, PageTest pageTest) : base(page, pageTest)
+        {
+
+        }
+
+        private IAPIRequestContext requestContext = null!;
+        private IAPIResponse response = null!;
+
+        public async Task LoginToAPI(string emailAPI, string passwordAPI, string environment)
+        {
+            var playwright = await Playwright.CreateAsync();
+            requestContext = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = $"{environment}",
+                IgnoreHTTPSErrors = true
+            });
+
+            response = await requestContext.PostAsync(url: "/login", new APIRequestContextOptions()
+            {
+                // this is a anonymous type
+                // get api field
+                DataObject = new
+                {
+                    email = emailAPI,
+                    password = passwordAPI
+                }
+            });
+
+            // Assert for response result
+            var jsonString = await response.JsonAsync();
+            var jsonResultString = await response.TextAsync();
+
+            LoginResponseField.Success = jsonString?.GetProperty("success").GetBoolean();
+            //PostField.ApiKey = jsonString?.GetProperty("results").GetProperty("apiKey").GetString()!;
+            //PostField.ClientId = jsonString?.GetProperty("results").GetProperty("clientId").GetString();
+            //PostField.SessionToken = jsonString?.GetProperty("results").GetProperty("sessionToken").GetString();
+            LoginResponseField.ResponseResults = jsonResultString;
+        }
+    }
+}
---
+++ automation-dph/src/Api/LoginResponseField.cs
@@ -0,0 +1,12 @@
+﻿namespace automation_dph.src.Api
+{
+    public class LoginResponseField
+    {
+        public static bool? Success { get; set; }
+        public static string? ApiKey { get; set; }
+        public static string? ClientId { get; set; }
+        public static string? SessionToken { get; set; }
+        public static string? ErrorResults { get; set; }
+        public static string? ResponseResults { get; set; }
+    }
+}
